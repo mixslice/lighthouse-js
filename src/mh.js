4131b366ab5098b0b11af9af6072bfb171eae8d7
Zeqing Zhang <zzq889@gmail.com>
2015-10-16 11:39:32 +0800

    pageshow/hide

--- src/mh.js
+++ src/mh.js
@@ -247,17 +247,27 @@ class Maihoo {
 
 const _mhq = window._mhq || '';
 const maihoo = new Maihoo(_mhq);
-maihoo.track('page start');
 
-const pageshow = () => maihoo.track('page start');
-const pagehide = () => maihoo.track('page end');
+const pagestart = () => maihoo.track('page start');
+const pageend = () => maihoo.track('page end');
 
 if (window.onpagehide || window.onpagehide === null) {
-  window.addEventListener('pageshow', pageshow, false);
-  window.addEventListener('pagehide', pagehide, false);
+  window.addEventListener('pageshow', pagestart, false);
+  window.addEventListener('pagehide', pageend, false);
 } else {
-  window.addEventListener('load', pageshow, false);
-  window.addEventListener('unload', pagehide, false);
+  window.addEventListener('load', pagestart, false);
+  window.addEventListener('unload', pageend, false);
 }
+
+const pageshow = () => maihoo.track('page show');
+const pagehide = () => maihoo.track('page hide');
+
+document.addEventListener('visibilitychange', () => {
+  if (document.hidden) {
+    pagehide();
+  } else {
+    pageshow();
+  }
+}, false);
 
 global.maihoo = maihoo;
