19a6fbed259f9e6c2a6a8d8e61de14105435ef15
Zeqing Zhang <zzq889@gmail.com>
2016-04-27 21:30:34 +0800

    add isomorphic fetch as external lib

--- webpack/webpack.config.umd.js
+++ webpack/webpack.config.umd.js
@@ -11,6 +11,20 @@ module.exports = {
     filename: 'lighthouse.umd.js',
     libraryTarget: "umd"
   },
+  externals: {
+    "isomorphic-fetch": {
+      root: 'isomorphic-fetch',
+      commonjs2: 'isomorphic-fetch',
+      commonjs: 'isomorphic-fetch',
+      amd: 'isomorphic-fetch'
+    },
+    "es6-promise": {
+      root: 'es6-promise',
+      commonjs2: 'es6-promise',
+      commonjs: 'es6-promise',
+      amd: 'es6-promise'
+    }
+  },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
@@ -19,14 +33,14 @@ module.exports = {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      output: {
-        comments: false
-      }
-    })
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false
+    //   },
+    //   output: {
+    //     comments: false
+    //   }
+    // })
   ],
   module: {
     loaders: [
